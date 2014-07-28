ef3de5e3ac52e42caab972107fd536bc1205cf5d
Howard Chen <howardxchen@gmail.com>
2014-07-28 16:48:35 -0700

    Finds the correct customers

---
+++ listMeController.java
@@ -0,0 +1,17 @@
+/** Controller for ListMe App. */
+global class ListMeController {
+
+    /** Returns all events. */
+    @RemoteAction
+    global static ListMe_Event__c[] getEvents() {
+    	ListMe_Event__c[] events= [SELECT Name, Id FROM ListMe_Event__c];
+    	return events;
+    }
+
+    @RemoteAction
+    /** Get customers that are in an event with Id EVENTID. */
+    global static ListMe_Customer__c[] getCustomers(Id eventId) {
+        ListMe_Customer__c[] customers = [SELECT Name, Status__c, Id, Lead__c FROM ListMe_Customer__c WHERE Event__c =: eventId];
+        return customers;
+    }
+}
