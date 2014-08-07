2f0f28a5c159a843d565febd57f5c889e64b46eb
Howard Chen <howardxchen@gmail.com>
2014-08-07 14:27:21 -0700

    Add a few tests

--- listMeControllerTest.java
+++ listMeControllerTest.java
@@ -5,6 +5,10 @@ public class listMeControllerTest {
         ListMe_Event__c event = new ListMe_Event__c(Name = 'First Event');
         event.Show_Wait_Time__c = true;
         insert event;
+        ListMe_Customer__c customer = new ListMe_Customer__c(Name = 'David', Event__c = event.Id, Active__c = true);
+        ListMe_Customer__c customer2 = new ListMe_Customer__c(Name = 'David2', Event__c = event.Id, Active__c = false);
+        insert customer;
+        insert customer2;
         return event.Id;
     }
 
@@ -13,5 +17,28 @@ public class listMeControllerTest {
         ListMe_Event__c[] events = listMeController.getEvents();
         System.assertEquals(1, events.size());
         System.assertEquals('First Event', events[0].Name);
+    }
+
+    static testmethod void testGetCustomers() {
+        Id eventId = setup();
+        ListMe_Customer__c[] customers = listMeController.getActiveCustomers(eventId);
+        System.assertEquals(1, customers.size());
+    }
+
+    static testmethod void testCreateCustomer() {
+        Id eventId = setup();
+        Contact cont = new Contact(FirstName='Henry', LastName='Lee');
+        listMeController.createCustomer(cont, eventId);
+        ListMe_Customer__c[] customers = listMeController.getActiveCustomers(eventId);
+        System.assertEquals(2, customers.size());
+    }
+
+    static testmethod void testRemoveCustomer() {
+        Id eventId = setup();
+        ListMe_Customer__c[] customers = listMeController.getActiveCustomers(eventId);
+        System.assertEquals(1, customers.size());
+        listMeController.removeCustomer(customers[0].Id, false);
+        customers = listMeController.getActiveCustomers(eventId);
+        System.assertEquals(0, customers.size());
     }
 }
